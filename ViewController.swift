6bfee4f2050c7ff8ac22cfd96f4296cdbd5e0ceb
nancyk25 <nancykim@Administrators-MacBook-Air-7.local>
2015-01-06 13:18:33 -0800

    TableView

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Nancy Kim on 1/2/15.
+//  Copyright (c) 2015 Udemy. All rights reserved.
+//
+
+import UIKit
+
+//hold down command and hover over delgate to see its properties
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
